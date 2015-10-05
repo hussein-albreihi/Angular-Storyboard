bfffa58388dc84d744017c3716970279a916a587
Max Tötterman <max.philip.totterman@gmail.com>
2015-10-05 14:41:38 +0200

    maincontroller logic added

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,6 +7,33 @@
  * # MainCtrl
  * Controller of the angularProjectApp
  */
-angular.module('angularProjectApp').controller('MainCtrl', function (userFactory){
+angular.module('angularProjectApp').controller('MainCtrl', function (itemFactory){
+  var items;
 
+  function onError(res) {
+    console.log('Error', res);
+  }
+
+  function getItems() {
+    itemFactory.getItems()
+      .then(function (res) {
+        items = res.title;
+      }, onError);
+  }
+
+  getItems();
+
+  this.createItem = function (title, desc) {
+    var item= {
+      title: title,
+      description: desc
+    };
+    itemFactory.createItem(item)
+      .then(getItems, onError);
+  };
+
+  this.deleteItem = function (id) {
+    itemFactory.deleteItem(id)
+      .then(getItems, onError);
+  };
 });
